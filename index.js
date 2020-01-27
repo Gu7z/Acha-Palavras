26448724cde26f0fe1ed9814fb99df81cf8b1673
Gustavo Ferri <48556195+Gu7z@users.noreply.github.com>
2020-01-27 19:35:05 -0300

    Novo criaMat e novo AchaLetras

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var resultado = ""
 var contador = 0
 
 //Criando todas as matrizes necessarias
-var matrizResult = criaMat(larg, alt, false)
-var alo = criaMat(larg, alt, false)
-var matriz = criaMat(larg, alt, false)
+var matrizResult = criaMat(larg, alt)
+var alo = criaMat(larg, alt)
+var matriz = criaMat(larg, alt)
 
 //Exemplo 
 matriz[6][0] = "g"
@@ -26,8 +26,13 @@ matriz[2][3] = "a"
 matriz[2][4] = "d"
 matriz[2][5] = "o"
 
+matriz[1][5] = "l"
+matriz[3][5] = "k"
+matriz[4][5] = "o"
+
 var palavra1 = "gustavo"
 var palavra2 = "avoado"
+var palavra3 = "loko"
 
 console.log(matriz) //Matriz com as palavras
 
@@ -39,28 +44,30 @@ console.log('------Resultado2------')
 
 achaLetra(matriz, palavra2)
 
+console.log('------Resultado3------')
+
+achaLetra(matriz, palavra3)
+
 console.log('------Acabou------')
 
 //Fim exemplo
 
 //Cria uma matriz zerada
 function criaMat(x, y, bool){
-  let matriz = [];
-  for (let i = 0; i < x; i++) {
-    let linha = [];
-    for (let j = 0; j < y; j++) {
-      if(bool){
-        linha.push(Math.random().toString(36).substring(2, 3))
-      }else{
-        linha.push("-")
-      }
+
+  var mat = new Array(x);
+
+  for (var i = 0; i < x; i++) {
+    mat[i] = new Array(y);
+    for (let j = 0; j < y; j++){
+      mat[i][j] = "-"
     }
-    matriz.push(linha);
   }
-  return matriz
+
+  return mat
 }
 
-//Func para zerar as variaveis
+//Func para zerar a matriz toda as vezes que rodar o codigo procurando
 function zerando(){
   contador = 0
   resultado = ""
@@ -69,7 +76,7 @@ function zerando(){
 //Funcao para copiar uma matriz resultante para a matriz total
 function juntaMatriz(matriz){
   for(i = 0; i < matriz.length; i++){
-    for (j = 0; j < matriz[0].length; j++){
+    for (j = 0; j < matriz[i].length; j++){
       if(matriz[i][j] != "-"){
         alo[i][j] = matriz[i][j]
       }
@@ -80,196 +87,169 @@ function juntaMatriz(matriz){
 
 //Func principal para achar a palavra na matriz
 function achaLetra(matriz, palavra){
+
+  possibleDir = {
+    
+    //Func para achar os elementos para cima
+    cima(i, j, matriz, palavra){
+      if(i-contador >= 0 && matriz[i-contador][j] == palavra[contador]){
+        resultado += matriz[i-contador][j]
+        contador += 1
+        if(resultado == palavra){
+          return true
+        }else if(contador > palavra.lenght){
+          return false
+        }else if(possibleDir['cima'](i, j, matriz, palavra)){
+          return true
+        }
+      }else{
+        return false
+      }
+    },
+      
+    //Func para achar os elementos para Cima -  Direita
+    cimaD(i, j, matriz, palavra){
+      if(i-contador >= 0 && j+contador < matriz[0].length && matriz[i-contador][j+contador] == palavra[contador]){
+        resultado += matriz[i-contador][j+contador]
+        contador += 1
+        if(resultado == palavra){
+          return true
+        }else if(contador > palavra.lenght){
+          return false
+        }else if(possibleDir['cimaD'](i, j, matriz, palavra)){
+          return true
+        }
+      }else{
+        return false
+      }   
+    },
+    
+    //Func para achar os elementos a direita
+    direita(i, j, matriz, palavra){
+      if(j+contador <= matriz[i].length && matriz[i][j+contador] == palavra[contador]){
+        resultado += matriz[i][j+contador]
+        contador += 1
+
+        if(resultado == palavra){
+          return true
+        }else if(contador > palavra.length){
+          return false
+        }else if(possibleDir['direita'](i, j, matriz, palavra)){
+          return true
+        }
+      }else{
+        return false
+      } 
+    },
+    
+    //Func para achar os elementos para Baixo - Direita
+    baixoD(i, j, matriz, palavra){
+      if(i+contador < matriz.length && j+contador < matriz[0].length && matriz[i+contador][j+contador] == palavra[contador]){
+          resultado += matriz[i+contador][j+contador]
+          contador += 1
+          if(resultado == palavra){
+            return true
+          }else if(contador > palavra.lenght){
+            return false
+          }else if(possibleDir['baixoD'](i, j, matriz, palavra)){
+            return true
+          }
+        }else{
+          return false
+        }  
+    },
+      
+    //Func para achar os elementos para baixo
+    baixo(i, j, matriz, palavra){
+      if(i+contador < matriz.length && matriz[i+contador][j] == palavra[contador]){
+        resultado += matriz[i+contador][j]
+        contador += 1
+        if(resultado == palavra){
+          return true
+        }else if(contador > palavra.lenght){
+          return false
+        }else if(possibleDir['baixo'](i, j, matriz, palavra)){
+          return true
+        }
+      }else{
+        return false
+      } 
+    },
+
+    //Func para achar os elementos para Baixo - Esquerda   
+    baixoE(i, j, matriz, palavra){
+      if(i+contador < matriz[0].length && j-contador >= 0 && matriz[i+contador][j-contador] == palavra[contador]){
+        resultado += matriz[i+contador][j-contador]
+        contador += 1
+        if(resultado == palavra){
+          return true
+        }else if(contador > palavra.lenght){
+          return false
+        }else if(possibleDir['baixoE'](i, j, matriz, palavra)){
+          return true
+        }
+      }else{
+        return false
+      }   
+    },
+    
+    //Func para achar os elementos a esquerda
+    esquerda(i, j, matriz, palavra){
+      if(j-contador >= 0 && matriz[i][j-contador] == palavra[contador]){
+        resultado += matriz[i][j-contador]
+        contador += 1
+        
+        if(resultado == palavra){
+          return true
+        }else if(contador > palavra.length){
+          return false
+        }else if(possibleDir['esquerda'](i, j, matriz, palavra)){
+          return true
+        }
+      }else {
+        return false
+      }  
+    },
+    
+    //Func para achar os elementos para Cima - Esquerda
+    cimaE(i, j, matriz, palavra){
+      if(i-contador >= 0 && j-contador >= 0 && matriz[i-contador][j-contador] == palavra[contador]){
+        resultado += matriz[i-contador][j-contador]
+        contador += 1
+        if(resultado == palavra){
+          return true
+        }else if(contador > palavra.lenght){
+          return false
+        }else if(possibleDir['cimaE'](i, j, matriz, palavra)){
+          return true
+        }
+      }else{
+        return false
+      }
+    },
+
+  }
+
   for (var i = 0; i < matriz.length; i++) {
     for (var j = 0; j < matriz[0].length; j++) {
       if(matriz[i][j] == palavra[0]){
-        if(direitaR(i, j, matriz, palavra)){
+        for (direction in possibleDir){
+          if(possibleDir[direction](i, j, matriz, palavra)){
+            matrizResult = criaMatResult(i, j, palavra, direction)
+          }
           zerando()
-          matrizResult = criaMatResult(i, j, palavra, "direita")
-        }else if(esquerdaR(i, j, matriz, palavra)){
-          zerando()
-          matrizResult = criaMatResult(i, j, palavra, "esquerda")
-        }else if(cimaR(i, j, matriz, palavra)){
-          zerando()
-          matrizResult = criaMatResult(i, j, palavra, "cima")
-        }else if(baixoR(i, j, matriz, palavra)){
-          zerando()
-          matrizResult = criaMatResult(i, j, palavra, "baixo")
-        }else if(bdR(i, j, matriz, palavra)){
-          zerando()
-          matrizResult = criaMatResult(i, j, palavra, "bd")
-        }else if(beR(i, j, matriz, palavra)){
-          zerando()
-          matrizResult = criaMatResult(i, j, palavra, "be")
-        }else if(cdR(i, j, matriz, palavra)){
-          zerando()
-          matrizResult = criaMatResult(i, j, palavra, "cd")
-        }else if(ceR(i, j, matriz, palavra)){
-          zerando()
-          matrizResult = criaMatResult(i, j, palavra, "ce")
-        }
-        zerando()
+        } 
       }
     }
   }
-  alo = juntaMatriz(matrizResult)
-  console.log(alo)
-}
+  //Matriz com resultados separados
+  //console.log(matrizResult)
 
-//Func para achar os elementos a esquerda
-function esquerdaR(i, j, matriz, palavra){
-  if(j-contador >= 0 && matriz[i][j-contador] == palavra[contador]){
-    resultado += matriz[i][j-contador]
-    contador += 1
-    
-    if(resultado == palavra){
-      return true
-    }else if(contador > palavra.length){
-      return false
-    }else if(esquerdaR(i, j, matriz, palavra)){
-      return true
-    }else{
-      return false
-    }
+  //Junta todas as matrizes resultantes em uma so
+  //matrizResult = juntaMatriz(matrizResult)
 
-  }else {
-    return false
-  }
-}
-
-//Func para achar os elementos a direita
-function direitaR(i, j, matriz, palavra){
-  if(j+contador <= matriz[i].length && matriz[i][j+contador] == palavra[contador]){
-    resultado += matriz[i][j+contador]
-    contador += 1
-
-    if(resultado == palavra){
-      return true
-    }else if(contador > palavra.length){
-      return false
-    }else if(direitaR(i, j, matriz, palavra)){
-      return true
-    }else{
-      return false
-    }
-
-  }else{
-    return false
-  }
-}
-
-//Func para achar os elementos para cima
-function cimaR(i, j, matriz, palavra){
-  if(i-contador >= 0 && matriz[i-contador][j] == palavra[contador]){
-    resultado += matriz[i-contador][j]
-    contador += 1
-    if(resultado == palavra){
-      return true
-    }else if(contador > palavra.lenght){
-      return false
-    }else if(cimaR(i, j, matriz, palavra)){
-      return true
-    }else{
-      return false
-    }
-  }else{
-    return false
-  }
-}
-
-//Func para achar os elementos para Baixo - Direita
-function bdR(i, j, matriz, palavra){
-  if(i+contador < matriz.length && j+contador < matriz[0].length && matriz[i+contador][j+contador] == palavra[contador]){
-    resultado += matriz[i+contador][j+contador]
-    contador += 1
-    if(resultado == palavra){
-      return true
-    }else if(contador > palavra.lenght){
-      return false
-    }else if(bdR(i, j, matriz, palavra)){
-      return true
-    }else{
-      return false
-    }
-  }else{
-    return false
-  }
-}
-
-//Func para achar os elementos para baixo
-function baixoR(i, j, matriz, palavra){
-  if(i+contador < matriz.length && matriz[i+contador][j] == palavra[contador]){
-    resultado += matriz[i+contador][j]
-    contador += 1
-    if(resultado == palavra){
-      return true
-    }else if(contador > palavra.lenght){
-      return false
-    }else if(baixoR(i, j, matriz, palavra)){
-      return true
-    }else{
-      return false
-    }
-  }else{
-    return false
-  }
-}
-
-//Func para achar os elementos para Cima - Esquerda
-function ceR(i, j, matriz, palavra){
-  if(i-contador >= 0 && j-contador >= 0 && matriz[i-contador][j-contador] == palavra[contador]){
-    resultado += matriz[i-contador][j-contador]
-    contador += 1
-    if(resultado == palavra){
-      return true
-    }else if(contador > palavra.lenght){
-      return false
-    }else if(ceR(i, j, matriz, palavra)){
-      return true
-    }else{
-      return false
-    }
-  }else{
-    return false
-  }
-}
-
-//Func para achar os elementos para Cima -  Direita
-function cdR(i, j, matriz, palavra){
-  if(i-contador >= 0 && j+contador < matriz[0].length && matriz[i-contador][j+contador] == palavra[contador]){
-    resultado += matriz[i-contador][j+contador]
-    contador += 1
-    if(resultado == palavra){
-      return true
-    }else if(contador > palavra.lenght){
-      return false
-    }else if(cdR(i, j, matriz, palavra)){
-      return true
-    }else{
-      return false
-    }
-  }else{
-    return false
-  }
-}
-
-//Func para achar os elementos para Baixo - Esquerda
-function beR(i, j, matriz, palavra){
-  if(i+contador < matriz[0].length && j-contador >= 0 && matriz[i+contador][j-contador] == palavra[contador]){
-    resultado += matriz[i+contador][j-contador]
-    contador += 1
-    if(resultado == palavra){
-      return true
-    }else if(contador > palavra.lenght){
-      return false
-    }else if(beR(i, j, matriz, palavra)){
-      return true
-    }else{
-      return false
-    }
-  }else{
-    return false
-  }
+  //Matriz com resultados todos juntos
+  //console.log(matrizResult)
 }
 
 //Func para criar uma matriz temporaria com o resultado atual, até por na matriz total
@@ -284,13 +264,13 @@ function criaMatResult(i, j, palavra, direcao){
       matResult[i][j+percorre] = palavra[percorre]
     }else if(direcao == "esquerda"){
       matResult[i][j-percorre] = palavra[percorre]
-    }else if(direcao == "bd"){
+    }else if(direcao == "baixoD"){
       matResult[i+percorre][j+percorre] = palavra[percorre]
-    }else if(direcao == "be"){
+    }else if(direcao == "baixoE"){
       matResult[i+percorre][j-percorre] = palavra[percorre]
-    }else if(direcao == "cd"){
+    }else if(direcao == "cimaD"){
       matResult[i-percorre][j+percorre] = palavra[percorre]
-    }else if(direcao == "ce"){
+    }else if(direcao == "cimaE"){
       matResult[i-percorre][j-percorre] = palavra[percorre]
     }
   }
